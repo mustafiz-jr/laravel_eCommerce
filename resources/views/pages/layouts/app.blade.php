65f9db3b3dff29e79fe9007f784284510965fa62
mustafiz_jr <mustafijjr@gmail.com>
2025-06-16 18:00:29 +0600

    s

--- resources/views/pages/layouts/app.blade.php
+++ resources/views/pages/layouts/app.blade.php
@@ -16,10 +16,10 @@
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{asset('customer/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('customer/css/owl.theme.default.css')}}">
+    <link rel="stylesheet" href="{{ asset('customer/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('customer/css/owl.theme.default.css') }}">
 
-    <link rel="stylesheet" href="{{asset('customers/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('customers/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('customers/css/style.css') }}">
 
 </head>
@@ -75,7 +75,6 @@
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                             </svg>
-
                             <span class="cart-count">2</span>
                         </a>
                     </div>
