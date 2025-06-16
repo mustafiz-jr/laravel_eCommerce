3890b692eca64e6fcbfd81b7333cec5ea8fa6acb
mustafiz_jr <mustafijjr@gmail.com>
2025-06-16 17:58:31 +0600

    css

--- resources/views/pages/layouts/app.blade.php
+++ resources/views/pages/layouts/app.blade.php
@@ -16,10 +16,10 @@
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/plugins/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/plugins/owl.theme.default.css">
+    <link rel="stylesheet" href="{{asset('customer/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('customer/css/owl.theme.default.css')}}">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('customers/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{ asset('customers/css/style.css') }}">
 
 </head>
